b0253b3a507ed156a98e35187a09c0fbf3163b65
avintro <119504876+avintro@users.noreply.github.com>
2024-03-15 00:16:00 +0530

    util code discussion

--- src/index.js
+++ src/index.js
@@ -4,3 +4,11 @@ require('dotenv').config()
 const connectDB = require('./db/index');
 
 connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running at port: ${process.env.PORT}`);
+        })
+    })
+    .catch((err) => {
+        console.log("mongodb failed !!! ", err);
+    })
