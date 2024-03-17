3ed1b0016e6540f4491ca75f851543a5afe87386
avintro <119504876+avintro@users.noreply.github.com>
2024-03-17 23:56:36 +0530

    add user and video mmodule

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -1,8 +1,8 @@
 class ApiResponse {
     constructor(statusCode, data, message = 'Success') {
-        this.statusCode = statusCode
-        this.data = data
-        this.message = messagee
-        this.success == statusCode < 400
+        this.statusCode = statusCode;
+        this.data = data;
+        this.message = message; // Fixed typo: 'messagee' to 'message'
+        this.success = statusCode < 400; // Fixed: used assignment operator instead of comparison operator
     }
 }
